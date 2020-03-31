f4a40d88312693fccdb06168ee7a23a54fa32b43
方鹏飞 <fangpf5@chinaunicom.cn>
2020-03-31 19:31:44 +0800

    first commit

---
+++ 674.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def findLengthOfLCIS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        maxlen = 1
+        numlen = len(nums)
+        temp
+        while numlen:
