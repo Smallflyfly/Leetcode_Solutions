5b8a4d710ea76cdb263ee5d4566894fbab1d5648
smallflyfly <fangpf5@chinaunicom.cn>
2022-03-18 15:43:59 +0800

    add file

---
+++ 160.相交链表.cpp
@@ -0,0 +1,64 @@
+/*
+ * @lc app=leetcode.cn id=160 lang=cpp
+ *
+ * [160] 相交链表
+ */
+
+#include<iostream>
+#include<map>
+#include<algorithm>
+
+using namespace std;
+
+// @lc code=start
+// Definition for singly-linked list.
+// struct ListNode {
+//     int val;
+//     ListNode *next;
+//     ListNode(int x) : val(x), next(NULL) {}
+// };
+
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        if (nullptr == headA || nullptr == headB) return nullptr;
+        ListNode *pa = headA, *pb = headB;
+        if (pa == pb) return pa;
+        pa = pa->next, pb = pb->next;
+        while (pa || pb) {
+            if (pa == pb) return pa;
+            if (nullptr == pa && nullptr == pb) {
+                break;
+            }
+            if (nullptr == pb) {
+                pb = headA;
+                pa =  pa->next;
+            } else if (nullptr == pa) {
+                pa = headB;
+                pb = pb->next;
+            } else {
+                pa = pa->next;
+                pb = pb->next;
+            }
+        }
+        return nullptr;
+    }
+};
+// @lc code=end
+
+/**
+3
+[3]
+[2,3]
+0
+1
+
+3 2 3
+2 3 3
+ 
+
+
+
+
+ * 
+ */
---
+++ 78.子集.cpp
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=78 lang=cpp
+ *
+ * [78] 子集
+ */
+
+#include<iostream>
+#include<vector>
+#include<map>
+#include<algorithm>
+
+using namespace std;
+
+// @lc code=start
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> res;
+        res.push_back(vector<int>{});
+        
+};
+// @lc code=end
+
