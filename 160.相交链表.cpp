6de259b7f3f9e6e6bc8e6e5e0bbb51e424553f43
smallflyfly <fangpf5@chinaunicom.cn>
2022-04-24 14:07:58 +0800

    代码提交

--- 160.相交链表.cpp
+++ 160.相交链表.cpp
@@ -12,11 +12,11 @@ using namespace std;
 
 // @lc code=start
 // Definition for singly-linked list.
-// struct ListNode {
-//     int val;
-//     ListNode *next;
-//     ListNode(int x) : val(x), next(NULL) {}
-// };
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
 
 class Solution {
 public:
